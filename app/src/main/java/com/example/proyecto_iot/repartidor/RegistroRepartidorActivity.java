e857bbc3e1ec3641f86aed0f430c9324ef297dc5
anapaula1125 <anapaula.hachi@hotmail.com>
2024-09-18 17:11:18 -0500

    vista: registro de repartidor + dialog

--- app/src/main/java/com/example/proyecto_iot/repartidor/RegistroRepartidorActivity.java
+++ app/src/main/java/com/example/proyecto_iot/repartidor/RegistroRepartidorActivity.java
@@ -1,15 +1,23 @@
 package com.example.proyecto_iot.repartidor;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import com.example.proyecto_iot.LoginActivity;
 import com.example.proyecto_iot.R;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class RegistroRepartidorActivity extends AppCompatActivity {
     @Override
@@ -23,8 +31,29 @@ public class RegistroRepartidorActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
-// Para que el primer elemento actúe como hint
+        // Para que el primer elemento actúe como hint
         spinner.setSelection(0, false);
+
+        EditText editTextBirthDate = findViewById(R.id.editTextBirthDate);
+        editTextBirthDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(RegistroRepartidorActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                editTextBirthDate.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
 
     }
 
@@ -33,4 +62,11 @@ public class RegistroRepartidorActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
+
+    public void abrirPagLogueoExitoso (View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.putExtra("showDialog", true);
+        startActivity(intent);
+    }
+
 }

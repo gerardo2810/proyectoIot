b18f72019844ae994fc97eb5788268d99bed8c32
gerardo2810 <a20206089@pucp.edu.pe>
2024-09-21 23:26:31 -0500

    hola

--- app/src/main/java/com/example/proyecto_iot/LoginActivity.java
+++ app/src/main/java/com/example/proyecto_iot/LoginActivity.java
@@ -15,7 +15,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_login);
 
         Intent intent = getIntent();
         if (intent.getBooleanExtra("showDialog", false)) {

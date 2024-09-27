2077c462c40733cda80f6b7a49864eeda6142217
andres <a20191450@pucp.edu.pe>
2024-09-27 13:31:36 -0500

    correcciones

--- app/src/main/java/com/example/proyecto_iot/superadmin/gestion_usuarios_superadmin.java
+++ app/src/main/java/com/example/proyecto_iot/superadmin/gestion_usuarios_superadmin.java
@@ -1,22 +1,41 @@
 package com.example.proyecto_iot.superadmin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.proyecto_iot.R;
-import com.example.proyecto_iot.databinding.SuperadminActivityGestionReportesBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class gestion_usuarios_superadmin extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        binding = SuperadminActivityGestionReportesBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.superadmin_activity_gestion_usuarios);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.navigation_usuarios) {
+                    Intent intentoUsuario = new Intent(gestion_usuarios_superadmin.this, gestion_usuarios_superadmin.class);
+                    startActivity(intentoUsuario);
+                    return true;
+                } else if (item.getItemId() == R.id.navigation_reportes) {
+                    Intent intentoReporte = new Intent(gestion_usuarios_superadmin.this, gestion_reportes_superadmin.class);
+                    startActivity(intentoReporte);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }

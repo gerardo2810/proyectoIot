4f8fc3c91d532f4d25d1504d20df308045d5a4e9
andres <a20191450@pucp.edu.pe>
2024-09-27 15:08:45 -0500

    pantallas 1/10

--- app/src/main/java/com/example/proyecto_iot/superadmin/gestion_reportes_superadmin.java
+++ app/src/main/java/com/example/proyecto_iot/superadmin/gestion_reportes_superadmin.java
@@ -1,26 +1,49 @@
 package com.example.proyecto_iot.superadmin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.proyecto_iot.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class gestion_reportes_superadmin extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.superadmin_activity_gestion_reportes);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        int selectedItemId = getIntent().getIntExtra("SELECTED_ITEM_ID", R.id.navigation_reportes);
+        bottomNavigationView.setSelectedItemId(selectedItemId);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Intent intent = null;
+                if (item.getItemId() == R.id.navigation_usuarios) {
+                    intent = new Intent(gestion_reportes_superadmin.this, gestion_usuarios_superadmin.class);
+                } else if (item.getItemId() == R.id.navigation_reportes) {
+                    intent = new Intent(gestion_reportes_superadmin.this, gestion_reportes_superadmin.class);
+                }
+                if (intent != null) {
+                    intent.putExtra("SELECTED_ITEM_ID", item.getItemId());
+                    startActivity(intent);
+                    finish();
+                    return true;
+                }
+                return false;
+            }
         });
     }
 }

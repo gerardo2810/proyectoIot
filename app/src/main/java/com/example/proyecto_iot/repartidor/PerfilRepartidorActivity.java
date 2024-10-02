df6edf4c581630a5332acde4465b60bd9588c722
anapaula1125 <anapaula.hachi@hotmail.com>
2024-10-02 18:32:47 -0500

    vista:perfil - rol:repartidor

---
+++ app/src/main/java/com/example/proyecto_iot/repartidor/PerfilRepartidorActivity.java
@@ -0,0 +1,16 @@
+package com.example.proyecto_iot.repartidor;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.proyecto_iot.R;
+
+public class PerfilRepartidorActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_repartidor);
+
+    }
+}

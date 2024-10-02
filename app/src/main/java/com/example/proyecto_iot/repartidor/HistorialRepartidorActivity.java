aa15ab90bc48444ba89bbe0d2dba9ea29e75713e
anapaula1125 <anapaula.hachi@hotmail.com>
2024-10-02 17:34:07 -0500

    vista: historial - rol:repartidor

---
+++ app/src/main/java/com/example/proyecto_iot/repartidor/HistorialRepartidorActivity.java
@@ -0,0 +1,16 @@
+package com.example.proyecto_iot.repartidor;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.proyecto_iot.R;
+
+public class HistorialRepartidorActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historial_repartidor);
+
+    }
+}

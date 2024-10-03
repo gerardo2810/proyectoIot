2a72ced2c7aec0cc40819c72ec7aba7d48de9538
milenelarosa <a20206456@pucp.edu.pe>
2024-10-03 01:38:21 -0500

    Avances

--- app/src/main/java/com/example/proyecto_iot/admin_restaurante/ViewRestaurantScheduleActivity.java
+++ app/src/main/java/com/example/proyecto_iot/admin_restaurante/ViewRestaurantScheduleActivity.java
@@ -1,6 +1,24 @@
 package com.example.proyecto_iot.admin_restaurante;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.proyecto_iot.R;
+
 public class ViewRestaurantScheduleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.restaurante_activity_horarios);
+
+        Button btnCloseRestaurant = findViewById(R.id.btn_close_restaurant);
+
+        btnCloseRestaurant.setOnClickListener(v -> {
+            // Lógica para cerrar el restaurante (puedes mostrar un dialogo de confirmación)
+            Toast.makeText(this, "Restaurante cerrado", Toast.LENGTH_SHORT).show();
+        });
+    }
 }

773ed837a751abd97f3d27f171c54e267ae31e89
anapaula1125 <anapaula.hachi@hotmail.com>
2024-10-24 04:06:22 -0500

    notificacion: pedido listo, rol: repartidor

--- app/src/main/java/com/example/proyecto_iot/repartidor/RecojoCurso1Activity.java
+++ app/src/main/java/com/example/proyecto_iot/repartidor/RecojoCurso1Activity.java
@@ -1,20 +1,53 @@
 package com.example.proyecto_iot.repartidor;
 
+import static android.Manifest.permission.POST_NOTIFICATIONS;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.proyecto_iot.R;
+import com.example.proyecto_iot.cliente.SeguimientoPedidoActivity;
 
 public class RecojoCurso1Activity extends AppCompatActivity {
+
+    private final String CHANNEL_ID = "order_ready_channel";
+    private Handler handler;
+    private Runnable runnable;
+    private int interval = 30 * 100; // 10 segundos en milisegundos
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recojo_curso_1);
+
+        // Crear canal de notificación
+        createNotificationChannel();
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+                ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) ==
+                        PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(RecojoCurso1Activity.this,
+                    new String[]{POST_NOTIFICATIONS},
+                    101);
+        }
+
+        // Iniciar despues de 10 segundos
+        iniciarNotificacion();
 
         Button boton1 = findViewById(R.id.button3);
         boton1.setOnClickListener(v -> {
@@ -31,12 +64,59 @@ public class RecojoCurso1Activity extends AppCompatActivity {
 
     }
 
+    private void iniciarNotificacion() {
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                sendNotification();
+            }
+        };
+        handler.postDelayed(runnable, interval); // despues de 10 segundos
+    }
+
     @SuppressWarnings("MissingSuperCall")
     @Override
     public void onBackPressed() {
         Intent intent = new Intent(this, InicioRepartidorActivity.class); // Regresar a la vistaInicio
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Limpia la pila de actividades
         startActivity(intent);
+    }
+
+    // Método para crear el canal de notificación (solo Android 8.0 o superior)
+    public void createNotificationChannel() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            CharSequence name = "Pedido listo para recojer channel";
+            String description = "Canal para notificaciones del pedido listo para recojer";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    // Método para enviar notificaciones
+    private void sendNotification() {
+        Intent intent = new Intent(this, RecojoCurso2Activity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.baseline_check_circle_outline_24) // Cambia este icono por uno propio
+                .setContentTitle("Pedido listo para recoger!")
+                .setContentText("Ya puedes acercarte! El pedido se encuentra listo para recogerlo :D")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true); // La notificación desaparece al hacer clic
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS)
+                == PackageManager.PERMISSION_GRANTED) {
+            notificationManager.notify(1, builder.build());
+        }
+
     }
 
 }
